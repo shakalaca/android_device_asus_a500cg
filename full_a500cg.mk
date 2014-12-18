b8608500b29afb0129b470d82d2dfec811bf7da3
Shaka Huang <shakalaca@gmail.com>
2014-12-18 12:36:56 +0800

    Correct APN settings
    Signed-off-by: Shaka Huang <shakalaca@gmail.com>

--- full_a500cg.mk
+++ full_a500cg.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
