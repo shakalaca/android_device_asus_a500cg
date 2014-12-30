8835643b2b3a34bf58c3d0203de71c1052c691e2
quanganh2627 <quanganh2627@gmail.com>
2014-12-30 23:53:09 +0700

    Modified for CM build
    Signed-off-by: quanganh2627 <quanganh2627@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+add_lunch_combo cm_a500cg-eng
 add_lunch_combo cm_a500cg-userdebug
 add_lunch_combo aosp_a500cg-userdebug
