92eebde271ee6c8b7af7455ded103b7761160583
Abiel Zulio M <abielzuliom@users.noreply.github.com>
2015-02-26 16:32:30 +0700

    a500cg: add display resolution for bootanimation

--- cm.mk
+++ cm.mk
@@ -10,6 +10,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/asus/a500cg/full_a500cg.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a500cg
 PRODUCT_NAME := cm_a500cg
